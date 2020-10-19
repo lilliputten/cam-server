f7ce4cc76864c7ef9ef22c40a9e25a9701ad0e47
lilliputten <lilliputten@yandex.ru>
2020-10-19 04:24:47 +0300

    Maintenance utils

--- util-venv-init.cmd
+++ util-venv-init.cmd
@@ -3,7 +3,7 @@ rem @desc Initialize python venv
 rem @changed 2020.10.19, 03:30
 
 python -m virtualenv -p "C:/Python27/python.exe" .venv
-call .venv/Scripts/activate
+REM  call .venv/Scripts/activate
 pip install -r requirements-dev.txt
 REM  call .venv/Scripts/deactivate
 
