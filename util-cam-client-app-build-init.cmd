6bfdb850dc0c368a5b674b201bd240a0308d95d7
lilliputten <lilliputten@yandex.ru>
2020-10-21 04:56:30 +0300

    Maintenance scripts

--- util-cam-client-app-build-init.cmd
+++ util-cam-client-app-build-init.cmd
@@ -1,6 +1,9 @@
 @echo off
 rem @desc Initialize `cam-client-app-build` link
-rem @changed 2020.10.19, 03:21
+rem @changed 2020.10.21, 04:55
 
+REM echo Using entire build repository
 REM linkd "cam-client-app-build" "../cam-client-app-build"
+
+echo Using publish folder of source repository
 linkd "cam-client-app-build" "../cam-client-app/publish"
