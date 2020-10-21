ed570b5081786ffc4aefc43a41aaa97ad2d8a5b0
lilliputten <lilliputten@yandex.ru>
2020-10-21 04:54:41 +0300

    Maintenance scripts

--- util-cam-client-app-build-init.cmd
+++ util-cam-client-app-build-init.cmd
@@ -2,4 +2,5 @@
 rem @desc Initialize `cam-client-app-build` link
 rem @changed 2020.10.19, 03:21
 
-linkd "cam-client-app-build" "../cam-client-app-build"
+REM linkd "cam-client-app-build" "../cam-client-app-build"
+linkd "cam-client-app-build" "../cam-client-app/publish"
