78654c7ea57485fb866619c828f0acd5a51d50e0
lilliputten <lilliputten@yandex.ru>
2020-10-23 23:42:46 +0300

    Python initializaion scripts

--- util-venv-init.cmd
+++ util-venv-init.cmd
@@ -1,9 +1,14 @@
 @echo off
 rem @desc Initialize python venv
-rem @changed 2020.10.19, 03:30
+rem @changed 2020.10.23, 23:41
 
+REM  Global system requirements...
+pip install setuptools virtualenv virtualenvwrapper-win
+REM  Create venv...
 python -m virtualenv -p "C:/Python27/python.exe" .venv
+REM  REM  Activate venv
 REM  call .venv/Scripts/activate
+REM  Install project dependencies...
 pip install -r requirements-dev.txt
 REM  call .venv/Scripts/deactivate
 
--- util-venv-init.sh
+++ util-venv-init.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # @desc Initialize python venv
-# @changed 2020.10.19, 03:30
+# @changed 2020.10.23, 23:41
 
 if uname | grep -q "CYGWIN"; then
   cmd /C "util-venv-init.cmd"
 else
+  # Global system requirements...
+  pip install setuptools virtualenv
+  # Create venv...
   python -m virtualenv -p "/usr/bin/python2.7" .venv
+  # Activate venv
   . ./.venv/Scripts/activate
+  # Install project dependencies...
   pip install -r requirements-dev.txt
 fi
